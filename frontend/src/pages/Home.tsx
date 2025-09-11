6d4283200cd5b5277c5fd5cbf9e25f7720388f1a
MileeDevWork <letrinh0904.work@gmail.com>
2025-09-12 01:07:09 +0700

    Create UI Register, Login page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,15 +1,35 @@
 import React from 'react'
 import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
 
 
 const Home = () => {
   return (
-    <div>
-        <h1 className="bg-amber-400 text-3xl font-bold underline">
-    Hello world!
-    <Button>Click me</Button>
-  </h1>
-    </div>
+    <div className="min-h-screen w-full relative bg-white">
+  {/* Cool Blue Glow Top */}
+  <div
+    className="absolute inset-0 z-0"
+    style={{
+      background: "#ffffff",
+      backgroundImage: `
+        radial-gradient(
+          circle at top center,
+          rgba(70, 130, 180, 0.5),
+          transparent 70%
+        )
+      `,
+      filter: "blur(80px)",
+      backgroundRepeat: "no-repeat",
+    }}
+  />
+     {/* Your Content/Components */}
+     <div className='relative z-10'>
+        <h1>
+          Hello world!
+        </h1>
+        <Input type="email" placeholder="Email" />
+      </div>
+</div>
   )
 }
 
---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import Logo from '../assets/logo.png'
+import Loginimg from '../assets/Login.jpg'
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from '@/components/ui/input'
+
+const Login = () => {
+  const form = useForm({
+    defaultValues: {
+      username: "",
+      email: "",
+      password: ""
+    }
+  });
+
+  return (
+    <div className="min-h-screen w-full relative bg-white">
+      {/* Cool Blue Glow Top */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          background: "#ffffff",
+          backgroundImage: `
+        radial-gradient(
+          circle at top center,
+          rgba(70, 130, 180, 0.5),
+          transparent 70%
+        )
+      `,
+          filter: "blur(80px)",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+      {/* Your Content/Components */}
+      <div className="relative z-10 flex w-screen min-h-screen items-center justify-center">
+        <div className=" flex w-4xl h-[600px] flex-row shadow-xl rounded-2xl">
+          <div className="basis-1/2 bg-center bg-no-repeat bg-cover rounded-l-2xl"
+            style={{ backgroundImage: `url(${Loginimg})` }}></div>
+          <div className="basis-1/2 flex items-center flex-col p-10 bg-white rounded-r-2xl">
+            <div><img src={Logo} alt="Logo" className="w-full h-full object-cover"></img></div>
+            <h2 className='pt-4'>Login</h2>
+            <Form {...form}>
+              <form className="space-y-8 pt-6">
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        <Input placeholder="Email" {...field} className='w-80 h-12' />
+                      </FormControl>
+                      <FormControl>
+                        <Input placeholder="Password" {...field} className='w-80 h-12' />
+                      </FormControl>
+                      <FormDescription>
+                        Don't have an account? Sign up
+                      </FormDescription>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <Button type="submit" className='w-80 h-12'>Login</Button>
+              </form>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Login
